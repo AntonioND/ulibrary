1a4e40ea28f0d80c3e087daf9691b2ec127308a8
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-07-25 18:29:15 +0100

    Add new draft build system and remove the old one

--- source/build.bat
+++
@@ -1,4 +0,0 @@
-set path=C:\devkitPro\devkitARM\bin;%path%
-rem make clean
-make
-pause > nul
--- source/clean.bat
+++
@@ -1,4 +0,0 @@
-set path=C:\devkitPro\devkitARM\bin;%path%
-make clean
-del *.ncb > nul
-del *.user > nul
